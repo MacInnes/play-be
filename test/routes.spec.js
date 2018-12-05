8474a8521aa171e73d7ebae1b21209625e08c724
MacInnes <califormula@gmail.com>
2018-12-05 10:32:21 -0700

    one endpoint connection tested

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -3,7 +3,13 @@ const should = chai.should();
 const chaiHttp = require('chai-http');
 const server = require('../app.js');
 
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
+
 chai.use(chaiHttp);
+
+// basic example
 
 describe('Client Routes', () => {
   it('can get root', done => {
@@ -14,4 +20,35 @@ describe('Client Routes', () => {
         done();
       });
   });
+});
+
+describe('API Routes', () => {
+  // db setup before each test
+
+  before((done) => {
+    database.migrate.latest()
+      .then( () => done() )
+      .catch(error => {
+        throw error;
+      });
+  });
+
+  beforeEach((done) => {
+    database.seed.run()
+      .then( () => done() )
+      .catch(error => {
+        throw error;
+      })
+  });
+
+  it('responds to /api/v1/songs', done => {
+    chai.request(server)
+      .get('/api/v1/songs')
+      .end((err, response) => {
+        response.should.have.status(200);
+        response.body.message.should.equal("Endpoint connected");
+        done();
+      });
+  });
+
 });
