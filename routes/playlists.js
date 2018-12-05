3d444d45cf5ec1d791bf491263dc0d93d6f3a20c
MacInnes <califormula@gmail.com>
2018-12-04 22:56:46 -0700

    confirms that song and playlist routes are connected

--- routes/playlists.js
+++ routes/playlists.js
@@ -3,5 +3,8 @@ const router  = express.Router();
 
 // router.get(endpoint, callback) goes here
 
+router.get('/', function(req, res, err){
+  res.status(200).json({message: "Endpoint connected"})
+})
 
 module.exports = router;
