5b3cc677f25d6d1808298424af473543c0d16daf
MacInnes <califormula@gmail.com>
2018-12-06 14:40:21 -0700

    slight refactor

--- routes/search.js
+++ routes/search.js
@@ -3,7 +3,9 @@ const router  = express.Router();
 const request = require('request');
 
 router.post('/', async function(req, res){
+
   var artist = req.body.artist;
+
   var promise = new Promise((resolve, reject) => {
     var options = {
       url: `http://api.musixmatch.com/ws/1.1/track.search?q_artist=${artist}&apikey=bf8a4b36f09e4df30ca2146f0821b791`,
@@ -11,17 +13,15 @@ router.post('/', async function(req, res){
         'Content-Type': 'application/json'
       }
     }
-
-    return request(options, function(err, res, body){
+    request(options, function(err, res, body){
       var jsonData = JSON.parse(body);
       var songData = jsonData.message.body.track_list;
-      console.log(songData);
       resolve(songData);
     });
   })
+  
   var songs = await promise;
-  console.log("ROUTE SONGS", songs)
-  if (songs){
+  if (songs.length){
     res.status(200).json(songs);
   } else {
     res.status(404).json({message: "No songs found by this artist"});
