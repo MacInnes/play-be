5f46eccdfe478a014c17f1bded8fbd8e36ef1bd3
MacInnes <califormula@gmail.com>
2018-12-05 17:05:01 -0700

    adds test environment to knex config file

--- knexfile.js
+++ knexfile.js
@@ -14,19 +14,14 @@ module.exports = {
     useNullAsDefault: true
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/play_test',
     migrations: {
-      tableName: 'knex_migrations'
+      directory: __dirname + './db/migrations'
+    },
+    seeds: {
+      directory: __dirname + './db/seeds/dev'
     }
   },
 
