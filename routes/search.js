96eba13742bcb5e62d60dbee5569e48aac9f7c55
MacInnes <califormula@gmail.com>
2018-12-06 14:56:53 -0700

    adds Song model to /search route output

--- routes/search.js
+++ routes/search.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router  = express.Router();
 const request = require('request');
+const Song = require('../models/song.js')
 
 router.post('/', async function(req, res){
 
@@ -8,7 +9,7 @@ router.post('/', async function(req, res){
 
   var promise = new Promise((resolve, reject) => {
     var options = {
-      url: `http://api.musixmatch.com/ws/1.1/track.search?q_artist=${artist}&apikey=bf8a4b36f09e4df30ca2146f0821b791`,
+      url: `http://api.musixmatch.com/ws/1.1/track.search?q_artist=${artist}&apikey=bf8a4b36f09e4df30ca2146f0821b791&page_size=100`,
       headers: {
         'Content-Type': 'application/json'
       }
@@ -19,13 +20,29 @@ router.post('/', async function(req, res){
       resolve(songData);
     });
   })
-  
+
   var songs = await promise;
-  if (songs.length){
-    res.status(200).json(songs);
+
+  // songs[0].track.track_id
+
+  var formatted = songs.map(function(song){
+    var title = song.track.track_name;
+    var artist = song.track.artist_name;
+    if (song.track.primary_genres.music_genre_list[0]){
+      var genre = song.track.primary_genres.music_genre_list[0].music_genre.music_genre_name;
+    } else {
+      var genre = "No Genre Supplied";
+    }
+    var rating = song.track.track_rating;
+    return new Song(title, artist, genre, rating);
+  })
+
+  if (formatted.length){
+    res.status(200).json(formatted);
   } else {
     res.status(404).json({message: "No songs found by this artist"});
   }
+
 });
 
 module.exports = router;
