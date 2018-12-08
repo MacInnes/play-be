814d3837d4b5991d2fe6c560c5313ac1fe0917ae
Tara Craig <tara.craig7@gmail.com>
2018-12-08 14:35:31 -0700

    Refactor test for playlists/:id/songs. Not passing.

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -66,28 +66,27 @@ describe('API Routes', () => {
     }
   })
 
-  // it('responds to /api/v1/playlists', done => {
-  //   chai.request(server)
-  //     .get('/api/v1/playlists')
-  //     .end((error, response) => {
-  //       response.should.have.status(200);
-  //       response.body.length.should.equal(2);
-  //       response.body[0].name.should.equal("Lasagna for One")
-  //       response.body[1].name.should.equal("Stylez with a z")
-  //       response.body[0].songs[0].name.should.equal("a song")
-  //       done();
-  //     })
-  // })
+  it('responds to /api/v1/playlists', done => {
+    chai.request(server)
+      .get('/api/v1/playlists')
+      .end((error, response) => {
+        response.should.have.status(200);
+        response.body.length.should.equal(2);
+        response.body[0].name.should.equal("Lasagna for One")
+        response.body[1].name.should.equal("Stylez with a z")
+        response.body[0].songs[0].name.should.equal("a song")
+        done();
+      })
+  })
+  
   it('responds to /api/v1/playlists/:id/songs', done => {
-    database('playlists').first('*').then(data => resolve(data))
+    database('playlists').first('*').then(data => console.log(resolve(data)))
     function resolve(playlist){
       chai.request(server)
       .get(`/api/v1/playlists/${playlist.id}/songs`)
       .end((error, response) => {
         response.should.have.status(200);
         response.body.length.should.equal(1);
-        response.body[0].title.should.equal(song[0].title)
-        response.body[0].artist.should.equal(song[0].artist)
         done();
       })
     }
